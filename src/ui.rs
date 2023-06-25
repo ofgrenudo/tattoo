1eb6560a78f4e2ee725ac1d8888577443eeab8e2
Joshua Winters-Brown <joshuawintersbrown@gmail.com>
2023-06-25 09:46:49 -0400

    Working UI with Auto generating fields

--- src/ui.rs
+++ src/ui.rs
@@ -1,15 +1,14 @@
-use native_windows_gui as nwg;
-use native_windows_derive as nwd;
-
+extern crate native_windows_gui as nwg;
+extern crate native_windows_derive as nwd;
 use nwd::NwgUi;
 use nwg::NativeUi;
 
-use tattoo::device;
-use tattoo::registry;
+use tattoo_lib::device;
+use tattoo_lib::registry;
+
 
 #[derive(Default, NwgUi)]
 pub struct TattooUI {
-    // Was 300x160
     #[nwg_control(size: (300, 230), position: (300, 300), title: "Tattoo", flags: "WINDOW|VISIBLE")]
     #[nwg_events( OnWindowClose: [TattooUI::safely_exit] )]
     window: nwg::Window,
@@ -48,7 +47,7 @@ impl TattooUI {
     fn confirm_input(&self) {
         nwg::simple_message("Confirmation", &format!("{}", "Tattoo was able to successfully submit the data."));
     }
-    
+
     fn safely_exit(&self) {
         nwg::stop_thread_dispatch();
     }
@@ -71,36 +70,43 @@ impl TattooUI {
 
 }
 
-pub fn start() {
-
-    let manufacturer = "";
-    let serial_number = "";
-    let model = "";    
+pub fn begin_ui() {
+    let mut _asset_tag = "".to_string();
+    let mut _manufacturer = "".to_string();
+    let mut _serial_number = "".to_string();
+    let mut _model = "".to_string();
 
     let registry_exists = registry::check_exists();
-
-
+    println!("{:?}", registry_exists);
 
     match registry_exists {
         Ok(()) => {
-            registry::assettag::get();
+            _asset_tag = registry::assettag::get();
+            _manufacturer = registry::make::get();
+            _serial_number = registry::serialnumber::get();
+            _model = registry::model::get();
         },
         Err(()) => {
-            let manufacturer = device::make::get();
-            let serial_number = device::serialnumber::get();
-            let model = device::model::get();    
+            _manufacturer = device::make::get();
+            _serial_number = device::serialnumber::get();
+            _model = device::model::get();
         },
     }
 
+    // Catch Null Values
+    if _manufacturer == "".to_string() { _manufacturer = device::make::get(); }
+    if _serial_number == "".to_string() { _serial_number = device::serialnumber::get(); }
+    if _model == "".to_string() { _model = device::model::get(); }
 
+    // Begin Loading App
     nwg::init().expect("Failed to init Native Windows GUI");
     let app: tattoo_u_i_ui::TattooUIUi = TattooUI::build_ui(Default::default()).expect("Failed to build UI");
 
     // Set Fields to Information Passed to Function Device
-    app.set_asset_tag("");
-    app.set_manufacturer(manufacturer);
-    app.set_serial_number(serial_number);
-    app.set_model(model);
+    app.set_asset_tag(&_asset_tag);
+    app.set_manufacturer(&_manufacturer);
+    app.set_serial_number(&_serial_number);
+    app.set_model(&_model);
 
     nwg::dispatch_thread_events();
 }
