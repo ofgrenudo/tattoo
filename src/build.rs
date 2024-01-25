474d4316f1131a3d80670aea240aac0812ac2fbb
Joshua Winters-Brown <joshuawintersbrown@gmail.com>
2024-01-25 16:12:59 -0500

    Added UI with Slint
    I was able to get Slint working where it could display the information needed for the app.
    
    There is a weird bug, wher when you run the program the console remains open.
    The other complaint about slint is that the UI is kind of ugly / hard to work with.
    A feature id like to add seperate the UI into its own UI folder etc, but i would need to do more practicing with slint ouside of the app.
    The final complaint is that I would like to add the abbility to update the values in the UI

---
+++ src/build.rs
@@ -0,0 +1,3 @@
+fn main() {
+    slint::compile("ui/AppWindow.slint").unwrap();
+}
--- src/main.rs
+++ src/main.rs
@@ -60,41 +60,137 @@ fn main() {
         let _ = registry::manufacturer::set(system::manufacturer::get().unwrap());
         let _ = registry::model::set(system::model::get().unwrap());
         let _ = registry::serial_number::set(system::serial_number::get().unwrap());
+        std::process::exit(0)
     }
     if args.manufacturer {
-        println!("manufacturer: {}", system::manufacturer::get().unwrap());
+        println!("{}", system::manufacturer::get().unwrap());
     }
     if args.model {
-        println!("Model: {}", system::model::get().unwrap());
+        println!("{}", system::model::get().unwrap());
     }
     if args.serial_number {
-        println!("Serial Number: {}", system::serial_number::get().unwrap());
+        println!("{}", system::serial_number::get().unwrap());
     }
     if args.get_asset_tag {
-        println!("Asset Tag: {}", registry::asset_tag::get().unwrap());
+        println!("{}", registry::asset_tag::get().unwrap());
     }
     if args.get_status {
-        println!("Status: {}", registry::status::get().unwrap());
+        println!("{}", registry::status::get().unwrap());
     }
 
     if args.status.is_some() {
         let _ = registry::status::set(args.status.unwrap_or("".to_string()));
+        std::process::exit(0)
     }
-
     if args.asset_tag.is_some() {
         let _ = registry::asset_tag::set(args.asset_tag.unwrap_or("".to_string()));
+        std::process::exit(0)
     }
 
-    // This is our default behavoiur, what will we do. Basically, since all values are default we will take the inverse of that.
-    // If any one value is set to true, then it will become false and this will into run.
+    //This is our default behavoiur, what will we do. Basically, since all values are default we will take the inverse of that.
+    //f any one value is set to true, then it will become false and this will into run.
     if !(args.serial_number || args.manufacturer || args.model || args.get_asset_tag || args.get_status) {
-        println!(
-            "Manufacturer: {}\nModel: {}\nSerial Number: {}\nStatus: {}\nAsset Tag: {}",
-            system::manufacturer::get().unwrap(),
-            system::model::get().unwrap(),
-            system::serial_number::get().unwrap(),
-            registry::status::get().unwrap_or("Null".to_string()),
-            registry::asset_tag::get().unwrap_or("Null".to_string())
-        );
+        // println!(
+        //     "Manufacturer: {}\nModel: {}\nSerial Number: {}\nStatus: {}\nAsset Tag: {}",
+        //     system::manufacturer::get().unwrap(),
+        //     system::model::get().unwrap(),
+        //     system::serial_number::get().unwrap(),
+        //     registry::status::get().unwrap_or("Null".to_string()),
+        //     registry::asset_tag::get().unwrap_or("Null".to_string())
+        // );
+        slint::slint! {
+            import { Button, VerticalBox} from "std-widgets.slint";
+    
+            export global SystemInformation { 
+                in property <string> manufacturer;
+                in property <string> model;
+                in property <string> serial_number;
+                in property <string> status;
+                in property <string> asset_tag;
+            }
+    
+            export component App inherits Window {
+                always-on-top: true;
+                default-font-size: 24px;
+                title: "Tattoo";
+                VerticalBox {
+                    padding: 50px;
+                    spacing: 12px;
+    
+                    HorizontalLayout {
+    
+                        Text { 
+                            text: "Manufacturer: ";
+                        }
+                        
+                        TextInput {
+                            text: SystemInformation.manufacturer;
+                            horizontal-alignment: right; 
+                            read-only: true;
+                        }
+                    }
+    
+                    HorizontalLayout {
+                        Text { 
+                            text: "Model Name:  ";
+                        }
+                        
+                        TextInput {
+                            text: SystemInformation.model;
+                            horizontal-alignment: right; 
+                            read-only: true;
+                        }
+                    }
+    
+                    HorizontalLayout {
+                        Text { 
+                            text: "Serial Number: ";
+                        }
+                        
+                        TextInput {
+                            text: SystemInformation.serial-number;
+                            horizontal-alignment: right; 
+                            read-only: true;
+                        }
+                    }
+    
+                    HorizontalLayout {
+                        Text { 
+                            text: "Status: ";
+                        }
+                        
+                        TextInput {
+                            text: SystemInformation.status;
+                            horizontal-alignment: right; 
+                            read-only: true;
+                        }
+                    }
+    
+                    HorizontalLayout {
+                        Text { 
+                            text: "Asset Tag: ";
+                        }
+                        
+                        TextInput {
+                            text: SystemInformation.asset-tag;
+                            horizontal-alignment: right; 
+                            read-only: true;
+                        }
+                    }
+                }
+            }
+        }
+    
+        let app = App::new().unwrap();
+        app.global::<SystemInformation>().set_manufacturer(system::manufacturer::get().unwrap_or("Null".to_string()).into());
+        app.global::<SystemInformation>().set_model(system::model::get().unwrap_or("Null".to_string()).into());
+        app.global::<SystemInformation>().set_serial_number(system::serial_number::get().unwrap_or("Null".to_string()).into());
+        app.global::<SystemInformation>().set_status(registry::status::get().unwrap_or("Null".to_string()).into());
+        app.global::<SystemInformation>().set_asset_tag(registry::asset_tag::get().unwrap_or("Null".to_string()).into());
+        // Example, Getting data.
+        // println!("{}", app.global::<SystemInformation>().get_asset_tag());
+        app.run().unwrap()    
     }
+
+    std::process::exit(1)
 }
